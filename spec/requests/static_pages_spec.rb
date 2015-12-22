16e80c537707033a7668c5b7097b4ef09d816043
Gleb Frolov <gleb.frolov@red-soft.biz>
2015-12-22 15:00:47 +0300

    Make a basic User model (including secure passwords)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,7 +7,7 @@ describe "Static pages" do
   describe "Home page" do
     before { visit root_path }
 
-    it { should have_content('Sample App') }
+    it { should have_content('Добро пожаловать в ГК') }
     it { should have_title(full_title('')) }
     it { should_not have_title('| Home') }
   end
@@ -15,21 +15,21 @@ describe "Static pages" do
   describe "Help page" do
     before { visit help_path }
 
-    it { should have_content('Help') }
-    it { should have_title(full_title('Help')) }
+    it { should have_content('Помощь') }
+    it { should have_title(full_title('Помощь')) }
   end
 
   describe "About page" do
     before { visit about_path }
 
-    it { should have_content('About') }
-    it { should have_title(full_title('About Us')) }
+    it { should have_content('О нас') }
+    it { should have_title(full_title('О нас')) }
   end
 
   describe "Contact page" do
     before { visit contact_path }
 
-    it { should have_content('Contact') }
-    it { should have_title(full_title('Contact')) }
+    it { should have_content('Контакты') }
+    it { should have_title(full_title('Контакты')) }
   end
 end
